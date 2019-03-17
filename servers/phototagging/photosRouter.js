ded7e12cbf5331594258df2ec00c7fb507860f40
Kari Nasu <knasu13@uw.edu>
2019-03-16 22:48:44 -0700

    Edited routes

--- servers/phototagging/photosRouter.js
+++ servers/phototagging/photosRouter.js
@@ -2,21 +2,20 @@ var express = require('express');
 var modules = require("./modules.js");
 var photosRouter = express.Router();
 
-photosRouter.post("/:photoID/:tagID", modules.tagOnPhoto);
-photosRouter.delete("/:photoID/:tagID", modules.tagOnPhoto);
 
-photosRouter.get("/:photoID", modules.specificPhoto); // GET /photos?tag={tagid} or GET /photos/:tagID
-
-// Like and unlike
-photosRouter.post("/:photoID", modules.specificPhoto);
 // Adding tag
 photosRouter.post("/:photoID/:tagID", modules.specificPhoto);
-// Delete photo
-photosRouter.delete("/:photoID", modules.specificPhoto);
 // Remove tag
 photosRouter.delete("/:photoID/:tagID", modules.specificPhoto);
 
-photosRouter.get("/", modules.photos); // Get all photos
+// Get photo
+photosRouter.get("/:photoID", modules.specificPhoto); // GET /photos?tag={tagid} or GET /photos/:tagID
+// Like and unlike
+photosRouter.post("/:photoID", modules.specificPhoto);
+// Delete photo
+photosRouter.delete("/:photoID", modules.specificPhoto);
+
+photosRouter.get("/", modules.photos);
 photosRouter.post("/", modules.photos);
 
 module.exports = photosRouter
