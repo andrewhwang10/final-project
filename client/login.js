fd4d4bc499481a4a76a17ee6cec4a37de2065304
Andrew Hwang <andrewhwang10@gmail.com>
2019-03-18 11:43:01 -0700

    Finished login response request

--- client/login.js
+++ client/login.js
@@ -1,36 +1,38 @@
-const LOGIN_FORM = $("#loginForm");
-const SIGNUP_FORM = $("#signupForm");
+const LOGIN_FORM = document.getElementById("loginForm");
+const SIGNUP_FORM = document.getElementById("signupForm");
 
 var params = {
     method: "POST",
     mode: 'cors',
     headers: { // Error when go run main.go (req. headers); Without it in docker, error (405 and not 200)
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, PUT, POST, PATCH, DELETE'
+        //'Access-Control-Allow-Origin': '*',
+        //'Access-Control-Allow-Methods': 'GET, PUT, POST, PATCH, DELETE'
+        "content-type": "application/json"
     }
 }
 
-LOGIN_FORM.submit(login);
-SIGNUP_FORM.submit(signUp);
+LOGIN_FORM.addEventListener("submit", login);
+SIGNUP_FORM.addEventListener("submit",  signUp)
 
 
 function login(e) {
     e.preventDefault();
     console.log("logging in...");
-    var data = JSON.stringify(LOGIN_FORM.serializeArray());
-    params.body = data;
+    
+    var data = new FormData(LOGIN_FORM);
+    params.body = JSON.stringify(makeJSONObject(data));
 
     fetch(BASE_URI + "/sessions", params)
     .then(response => {
         if (!response.ok) {
             throw new Error('HTTP error, status = ' + response.status);
         }
+        sid = response.headers.get(headerAuthorization);
+        sessionStorage.setItem('sessionID', sid);
         return response.json()
     })
     .then(r => {
         console.log("Success: ", r); // File param is empty
-        sid = r.Headers.get(headerAuthorization)
-        sessionStorage.setItem('sessionID', sid);
     })
     .catch(function(err) {
         console.log(err)
@@ -40,22 +42,34 @@ function login(e) {
 function signUp(e) {
     e.preventDefault();
     console.log("signing up...");
-    var data = JSON.stringify(SIGNUP_FORM.serializeArray());
-    params.body = data;
+    
+    var data = new FormData(SIGNUP_FORM);
+    console.log(makeJSONObject(data));
+    params.body = JSON.stringify(makeJSONObject(data));
 
     fetch(BASE_URI + "/users", params)
     .then(response => {
         if (!response.ok) {
             throw new Error('HTTP error, status = ' + response.status);
         }
+        console.log(response);
+        sid = response.headers.get(headerAuthorization);
+        sessionStorage.setItem('sessionID', sid);
         return response.json()
     })
     .then(r => {
         console.log("Success: ", r); // File param is empty
-        sid = r.Headers.get(headerAuthorization)
-        sessionStorage.setItem('sessionID', sid);
     })
     .catch(function(err) {
         console.log(err)
     });
+}
+
+
+function makeJSONObject(formData) {
+    var object = {};
+    formData.forEach(function(value, key){
+        object[key] = value;
+    });
+    return object;
 }
